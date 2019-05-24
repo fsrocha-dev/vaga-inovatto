b68063507adaeba475e596494026695117e52bb7
fsrocha-dev <contato.frankrocha@gmail.com>
2019-05-24 10:32:27 -0300

    Adicionando Quasar ao projeto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './styles/quasar.styl'
+import '@quasar/extras/material-icons/material-icons.css'
+import {
+  Quasar, 
+  QLayout,
+  QHeader,
+  QDrawer,
+  QPageContainer,
+  QPage,
+  QToolbar,
+  QToolbarTitle,
+  QBtn,
+  QIcon,
+  QList,
+  QItem,
+  QItemSection,
+  QItemLabel,
+} from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: {
+    QLayout,
+    QHeader,
+    QDrawer,
+    QPageContainer,
+    QPage,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QItem,
+    QItemSection,
+    QItemLabel,
+  },
+  directives: {
+  },
+  plugins: {
+  }
+ })
+
 Vue.config.productionTip = false
 
 new Vue({
